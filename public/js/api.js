f537479eab8d45e113be79be6c909f6ccbf76731
jeffyboyse <bestjeff03@gmail.com>
2026-09-08 13:52:11 +0200

    created a function that checks if the search input is empty and returns early if it is. This prevents unnecessary API calls when the input is empty.

--- public/js/api.js
+++ public/js/api.js
@@ -1,14 +1,19 @@
+let searchTerm = '';
 const input = document.querySelector('#searchInput');
 const button = document.querySelector('#searchBtn');
 const gallery = document.querySelector('#gallery');
 
-button.addEventListener('click', async () => {
+async function performSearch(){
     const search = encodeURIComponent(input.value.trim() || 'flower');
+
+    if (search === '') {
+        return;
+    }
 
     try {
         const response = await fetch(`/api.php?q=${search}`);
         const data = await response.json();
-
+        
         if (!response.ok || data.error) {
             throw new Error(data.error || 'Request failed');
         }
@@ -19,4 +24,14 @@ button.addEventListener('click', async () => {
     } catch (error) {
         gallery.textContent = `Error loading images: ${error.message}`;
     }
+
+};
+
+input.addEventListener('keydown', (e) =>{
+    if (e.key === "Enter") {
+        performSearch();
+    }
 });
+
+button.addEventListener('click', performSearch);
+
