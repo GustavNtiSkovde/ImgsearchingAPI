08cc5deef60dc0388fcc85a413ca3d47b4590987
Drekk <201110792+GustavNtiSkovde@users.noreply.github.com>
2026-09-09 15:11:11 +0200

    Fixed form and link error

--- public/index.php
+++ public/index.php
@@ -14,10 +14,10 @@
 <body>
 
   <header class="top-bar">
-    <div class="search-wrapper">
+    <form id="searchForm" class="search-wrapper">
       <input type="text" id="searchInput" class="search-input" placeholder="Search..." />
-      <button type="button" id="searchSubmit" class="enter-btn">↵</button>
-    </div>
+      <button type="submit" id="searchSubmit" class="enter-btn">↵</button>
+    </form>
 
     <div class="filter-wrapper">
       <div class="scale-slider-wrapper">
@@ -91,8 +91,5 @@
       <script src="js/leafletmap.js"></script> <!-- Script file has to come after the css, link to js and the div element -->
     </aside>
   </main>
-
-  <script src="api.js"></script>
-  <script src="ui.js"></script>
 </body>
 </html>
